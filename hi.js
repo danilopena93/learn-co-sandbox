b8a09d595b499e1c3dcf6317b21fb54c7d4e3e95
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-08 15:26:29 +0000

    automatically backed up by learn

---
+++ hi.js
@@ -0,0 +1,15 @@
+// function say(greeting, firstName) {
+//   console.log('I was called!')
+//   return `${greeting}, ${firstName}!`
+//   console.log('I was called!')
+// }
+
+// say("hi","danny")
+
+function speaker() {
+  var sentence = 'Bird is the word.';
+ 
+  console.log(sentence);
+}
+
+speaker()
